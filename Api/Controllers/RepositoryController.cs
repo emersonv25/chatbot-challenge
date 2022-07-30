5c4c2bb1268ce008a63eefc881c19f4361dda067
emersonv25 <emersondejesussantos@hotmail.com>
2022-07-30 18:29:56 -0300

    criação da API

---
+++ Api/Controllers/RepositoryController.cs
@@ -0,0 +1,37 @@
+﻿using api_git.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace api_git.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepositoryController : ControllerBase
+    {
+        // GET: api/<RepositoryController>
+        [HttpGet]
+        public List<Repository> Get(string userLogin = "takenet", string sort = "created_at", int perPage = 5, string order = "asc", string language = "c#")
+        {
+            List<Repository> repository = new List<Repository>();
+            RestClient client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/users/" + userLogin + "/repos?sort=" + sort + "&per_page=200&direction=" + order + "&page=1}", Method.Get);
+            var response = client.Execute(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                JArray jarray = JArray.Parse(response.Content);
+                string json = response.Content;
+                repository = JsonConvert.DeserializeObject<List<Repository>>(json);
+                repository = repository
+                    .Where(x => !string.IsNullOrWhiteSpace(x.language) && x.language.ToLower().Equals(language))
+                    .Take(perPage)
+                    .ToList();
+            }
+            return repository;
+        }
+    }
+}
---
+++ Api/Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_git.Models
+{
+    public class Repository
+    {
+        public string avatar_url { get; set; }
+        public string full_name { get; set; }
+        public string description { get; set; }
+        public string created_at { get; set; }
+        public string language { get; set; }
+        public Owner owner { get; set; }
+    }
+    public class Owner
+    {
+        public string login { get; set; }
+        public string avatar_url { get; set; }
+    }
+}
